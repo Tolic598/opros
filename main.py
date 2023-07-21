82212a2f48607fe4d1118ce9da9903fa614eeed9
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2023-07-21 20:16:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,42 @@
-
 import logging
-import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import Poll, PollType
 
-# Установите ваш токен Telegram бота
-BOT_TOKEN = '5018144842:AAHmknEVURWrdtgLXJ6d4ol9PVlYwEvLN2w'
-
-logging.basicConfig(level=logging.INFO)
+# Вставьте сюда ваш токен
+TOKEN = "5018144842:AAHmknEVURWrdtgLXJ6d4ol9PVlYwEvLN2w"
 
 # Инициализация бота и диспетчера
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-
-# Обработчик команды /create_poll
-@dp.message_handler(commands=['create_poll'])
-async def create_poll(message: types.Message):
-    # Вопрос для опроса
-    question = "Выберите ваш любимый вариант:"
-    # Варианты ответов для опроса
-    options = [
-        "Вариант 1", "Вариант 2", "Вариант 3", "Вариант 4", "Вариант 5",
-        "Вариант 6", "Вариант 7", "Вариант 8", "Вариант 9", "Вариант 10",
-        "Вариант 11", "Вариант 12", "Вариант 13", "Вариант 14", "Вариант 15",
-        "Вариант 16", "Вариант 17", "Вариант 18", "Вариант 19", "Вариант 20",
-        "Вариант 21", "Вариант 22", "Вариант 23", "Вариант 24", "Вариант 25",
-        "Вариант 26", "Вариант 27", "Вариант 28", "Вариант 29", "Вариант 30",
-        "Вариант 31", "Вариант 32",
-    ]
-
-    # Создаем объект опроса
-    poll = types.Poll(
-        question=question,
-        options=options,
-        type=types.PollType.QUIZ,
-        correct_option_id=0  # Номер варианта, который считается верным (начиная с 0)
-    )
-
-    # Отправляем опрос пользователю
-    try:
-        sent_poll = await message.answer_poll(poll=poll, is_anonymous=False)
-        logging.info(f"Опрос создан с ID: {sent_poll.poll.id}")
-    except Exception as e:
-        logging.error(f"Ошибка при создании опроса: {e}")
-        await message.answer("Произошла ошибка при создании опроса. Пожалуйста, попробуйте позже.")
-
+dp.middleware.setup(LoggingMiddleware())
 
 # Обработчик команды /start
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Привет! Я бот для создания опросов. Используйте команду /create_poll, чтобы создать опрос.")
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я бот для создания опросов. Введите ваши варианты через запятую:")
 
+# Обработчик текстового сообщения с вариантами опроса
+@dp.message_handler(lambda message: message.text.count(',') >= 31)
+async def create_poll(message: types.Message):
+    # Разбиваем варианты опроса на отдельные элементы
+    options = message.text.split(',')
+    options = [option.strip() for option in options]
 
-# Обработчик неизвестных команд
-@dp.message_handler()
-async def unknown_command(message: types.Message):
-    await message.reply("Извините, я не понимаю данную команду. Попробуйте другую или используйте /create_poll.")
-
+    # Проверяем, что количество вариантов не превышает 32 (максимум для опросов в Telegram)
+    if len(options) <= 32:
+        poll = Poll(
+            type=PollType.QUIZ,
+            question="Выберите один из вариантов:",
+            options=options,
+            correct_option_id=0  # Номер правильного варианта (в данном случае - первый)
+        )
+        await message.reply_poll(poll, disable_notification=True)
+    else:
+        await message.reply("Извините, максимальное количество вариантов для опроса - 32.")
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    executor.start_polling(dp, loop=loop, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    # Запуск бота
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
